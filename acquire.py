384a9b7aca21acba83e26c3ab4de89c0348a0c46
Corey Solitaire <corey.solitaire@gmail.com>
2020-10-23 09:16:52 -0500

    Exercise Complete

--- acquire.py
+++ acquire.py
@@ -29,3 +29,25 @@ def get_csv_url_data(file_name, url, cached=False):
         df = pd.read_csv(file_name, index_col=0)
     return df
 
+def api_request():
+    '''
+    This function retrieves all items of all pages
+    from the base url api and saves it as a dataframe
+    '''
+    #saving the base url
+    base_url = 'https://python.zach.lol'
+    #opening/visiting the url
+    response = requests.get('https://python.zach.lol/api/v1/sales')
+    #saving the data as a json script
+    data = response.json()
+    #saving the json script as a dataframe
+    df = pd.DataFrame(data['payload']['sales'])
+    #creating a loop to scan all the items pages
+    for number in range(1, data['payload']['max_page']):
+        #moving into the next page of items
+        response = requests.get(base_url + data['payload']['next_page'])
+        #saving the as a json script
+        data = response.json()
+        #adding the next page of items onto the original dataframe
+        df = pd.concat([df, pd.DataFrame(data['payload']['sales'])], ignore_index=True)
+    return df
