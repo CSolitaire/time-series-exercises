5f817089429844643df84fb3802cbc2c493cf5cd
Corey Solitaire <corey.solitaire@gmail.com>
2020-10-26 09:17:41 -0500

    Added Walkthrough Functions

--- acquire.py
+++ acquire.py
@@ -51,3 +51,62 @@ def api_request():
         #adding the next page of items onto the original dataframe
         df = pd.concat([df, pd.DataFrame(data['payload']['sales'])], ignore_index=True)
     return df
+
+def api_request():
+    '''
+    This function retrieves all items of all pages
+    from the base url api and saves it as a dataframe
+    '''
+    #saving the base url
+    base_url = 'https://python.zach.lol'
+    #opening/visiting the url
+    response = requests.get('https://python.zach.lol/api/v1/sales')
+    #saving the data as a json script
+    data = response.json()
+    #saving the json script as a dataframe
+    df = pd.DataFrame(data['payload']['sales'])
+    #creating a loop to scan all the items pages
+    for number in range(1, data['payload']['max_page']):
+        #moving into the next page of items
+        response = requests.get(base_url + data['payload']['next_page'])
+        #saving the as a json script
+        data = response.json()
+        #adding the next page of items onto the original dataframe
+        df = pd.concat([df, pd.DataFrame(data['payload']['sales'])], ignore_index=True)
+    return df
+
+def get_store_data():
+    """
+    This function checks for csv files
+    for items, sales, stores, and big_df 
+    if there are none, it creates them.
+    It returns one big_df of merged dfs.
+    """
+    # check for csv files or create them
+    if os.path.isfile('items.csv'):
+        items_df = pd.read_csv('items.csv', index_col=0)
+    else:
+        items_df = get_df('items')
+        
+    if os.path.isfile('stores.csv'):
+        stores_df = pd.read_csv('stores.csv', index_col=0)
+    else:
+        stores_df = get_df('stores')
+        
+    if os.path.isfile('sales.csv'):
+        sales_df = pd.read_csv('sales.csv', index_col=0)
+    else:
+        sales_df = get_df('sales')
+        
+    if os.path.isfile('big_df.csv'):
+        df = pd.read_csv('big_df.csv', index_col=0)
+        return df
+    else:
+        # merge all of the DataFrames into one
+        df = pd.merge(sales_df, stores_df, left_on='store', right_on='store_id').drop(columns={'store'})
+        df = pd.merge(df, items_df, left_on='item', right_on='item_id').drop(columns={'item'})
+
+        # write merged DateTime df with all data to directory for future use
+        df.to_csv('big_df.csv')
+        return df
+
